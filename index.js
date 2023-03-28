65fdd90bcdd2a8d586af81059eee55080554df2d
Giselle Smith <gisellec60@gmail.com>
2023-03-28 18:25:05 -0400

    reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(function (countBatteries, item) {
+    return countBatteries += item;
+}, 0)
+ 
+console.log(totalBatteries)// Code your solution here
